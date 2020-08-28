f5ce527a6add09d0cf3fda8ff61bbdb9624afcf8
Dan <daniel@missud.net>
2020-08-28 19:08:04 +0200

    Premier test de composant

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +6,42 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'guiYDEApp';
+  isAuth = false;
+  lastUpdate = new Promise((resolve, reject) => {
+    const date = new Date();
+    setTimeout(
+      () => {
+        resolve(date);
+      }, 2000
+    );
+  });
+  posts = [
+    {
+      title: 'Ground control to Captain Angular',
+      content: 'My first step with angular ... dqsdq dqs dqs defzefe feiazàçe a ezdçij doziej' +
+        'aopizjd doij dopizja doi  zdoizad dfpoza dzao. LJljdlkqsj ijij da piej ao zaihdfiuezo' +
+        'dadza zdoiazj d djzioa d  odiazjd oijd apzjj ijd zaijduebfoazij dzoiaddbeazij pazoijd d.',
+      dateCreation : '05/12/2020, 11:00',
+      nbClick: 0
+    },
+    {
+      title: 'it\'s Coffe time',
+      content: 'My coffee with angular ... dqsdq dqs dqs defzefe feiazàçe a ezdçij doziej' +
+        'aopizjd doij dopizja doi  zdoizad dfpoza dzao. LJljdlkqsj ijij da piej ao zaihdfiuezo' +
+        'dadza zdoiazj d djzioa d  odiazjd oijd apzjj ijd zaijduebfoazij dzoiaddbeazij pazoijd d.',
+      dateCreation : '05/10/2020, 23:45',
+      nbClick: 0
+    },
+    {
+      title: 'A other day withs Spring Boot',
+      content: '30 days with Spring Boot and  ... dqsdq dqs dqs defzefe feiazàçe a ezdçij doziej' +
+        'aopizjd doij dopizja doi  zdoizad dfpoza dzao. LJljdlkqsj ijij da piej ao zaihdfiuezo' +
+        'dadza zdoiazj d djzioa d  odiazjd oijd apzjj ijd zaijduebfoazij dzoiaddbeazij pazoijd d.',
+      dateCreation : '05/04/2020, 06:34',
+      nbClick: 0
+    }
+  ];
+
+  constructor() {
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MyPostsComponent } from './my-posts/my-posts.component';
+import {FormsModule} from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MyPostsComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
