140b1581e9effc32ea9931fa7adaf9368d28d081
dmissud <daniel@missud.net>
2020-10-10 11:47:43 +0200

    Restructuration de la communication entre le component Application et itSolution

--- src/app/application/view/itsolution/itsolution.component.ts
+++ src/app/application/view/itsolution/itsolution.component.ts
@@ -1,31 +1,17 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {Application} from '../../model/Application';
-import {Observable, Subscription} from 'rxjs';
-import {ApplicationService} from '../../service/application.service';
+import {Component, Input, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-itsolution',
   templateUrl: './itsolution.component.html',
   styleUrls: ['./itsolution.component.scss']
 })
-export class ItsolutionComponent implements OnInit, OnDestroy {
+export class ItsolutionComponent implements OnInit {
   @Input() itSolution;
-  application: Application;
-  application$: Observable<Application>;
 
-  private applicationSubsciption: Subscription;
-
-  constructor(private applicationService: ApplicationService) {
+  constructor() {
   }
 
   ngOnInit(): void {
-    this.application$ = this.applicationService.applicationObservable;
-
-    this.applicationSubsciption = this.applicationService.applicationObservable.subscribe(response => this.application = response);
   }
 
-  ngOnDestroy(): void {
-    this.applicationSubsciption.unsubscribe();
-
-  }
 }
