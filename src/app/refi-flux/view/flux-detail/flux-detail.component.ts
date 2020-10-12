538dfca8abde66e684f537567bba0c59f27f814a
dmissud <daniel@missud.net>
2020-10-12 09:42:03 +0200

    Version du module flux-refi avec liste des flux (file non add)

---
+++ src/app/refi-flux/view/flux-detail/flux-detail.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-flux-detail',
+  templateUrl: './flux-detail.component.html',
+  styleUrls: ['./flux-detail.component.scss']
+})
+export class FluxDetailComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
