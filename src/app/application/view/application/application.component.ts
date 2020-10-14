99a7ef26738683a6ceee121c68207f26e290ae81
dmissud <daniel@missud.net>
2020-10-14 08:29:05 +0200

    Ajout du routage vers ecran d'accueil quand pas d'application

--- src/app/application/view/application/application.component.ts
+++ src/app/application/view/application/application.component.ts
@@ -2,6 +2,7 @@ import {Component, OnDestroy, OnInit} from '@angular/core';
 import {Observable, Subscription} from 'rxjs';
 import {Application} from '../../model/Application';
 import {ApplicationService} from '../../service/application.service';
+import {Router} from '@angular/router';
 
 
 @Component({
@@ -16,13 +17,21 @@ export class ApplicationComponent implements OnInit, OnDestroy {
 
   private applicationSubsciption: Subscription;
 
-  constructor(private applicationService: ApplicationService) {
+  constructor(private applicationService: ApplicationService,
+              private router: Router) {
   }
 
   ngOnInit(): void {
     this.application$ = this.applicationService.applicationObservable;
 
-    this.applicationSubsciption = this.applicationService.applicationObservable.subscribe(response => this.application = response);
+    this.applicationSubsciption = this.applicationService.applicationObservable.subscribe(response => {
+      this.application = response;
+      if ((this.application === null)
+        || (this.application.codeApplication === null)
+        || (!this.application.codeApplication.includes('AP'))) {
+        this.router.navigate(['/']);
+      }
+    });
   }
 
   ngOnDestroy(): void {
